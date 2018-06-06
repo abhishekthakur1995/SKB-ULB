fa60eb38cc8a157e5edbb4fdb6875edd87807181
abhishekthakur1995 <abhishekthakur.oneplus5@gmail.com>
2018-06-07 00:01:27 +0530

    Revert "#rmoved eternal links."
    This reverts commit dfffdf2d03f0f53655f44d528c21ee06513f9314.

--- header.php
+++ header.php
@@ -7,15 +7,17 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/mdb.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.0/css/mdb.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/css/style.css">
-    <script type="text/javascript" src="<?php echo BASE_URL; ?>/js/jquery.min.js"></script>
-    <script type="text/javascript" src="<?php echo BASE_URL; ?>/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="<?php echo BASE_URL; ?>/js/mdb.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.13.0/umd/popper.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.0/js/mdb.min.js"></script>
 </head>
 <body>
+
 <nav class="mb-1 navbar navbar-expand-lg navbar-dark info-color header-height">
     <a class="navbar-brand header-font-size" href="<?php echo BASE_URL; ?>/dashboard.php">
         <span><?php echo $lang['company']; ?></span>
