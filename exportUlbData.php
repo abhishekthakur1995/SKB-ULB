4599050965c4a847ba9f4687b88ece63411ef3ed
abhishekthakur1995 <abhishekthakur.oneplus5@gmail.com>
2018-06-12 21:27:14 +0530

    #added fields in export list

--- exportUlbData.php
+++ exportUlbData.php
@@ -20,7 +20,7 @@ if($_SESSION['user_role'] == 'ULBADMIN') {
 	header("location: error.php?err_msg=Access Not Allowed");
 }
  
-$sql = "SELECT name, guardian, permanentAddress, temporaryAddress, dob, phoneNumber, birthPlace, district, ulbRegion, maritialStatus, receiptNumber, userFormValid, remark, specialPreference FROM candidate_list WHERE status = 0 AND ulbRegion = '".$_SESSION['ulb_region']."'";
+$sql = "SELECT name, guardian, permanentAddress, temporaryAddress, dob, phoneNumber, birthPlace, district, ulbRegion, category, gender, maritialStatus, receiptNumber, userFormValid, specialPreference, remark FROM candidate_list WHERE status = 0 AND ulbRegion = '".$_SESSION['ulb_region']."'";
 
 if (!$result = mysqli_query($link, $sql)) {
     exit(mysqli_error($link));
@@ -37,9 +37,10 @@ if (mysqli_num_rows($result) > 0) {
     	} else {
     		$row['userFormValid'] = $lang['under_scrutiny'];
     	}
-
     	$row['maritialStatus'] = $lang[$row['maritialStatus']];
     	$row['receiptNumber'] = substr($row['receiptNumber'], strpos($row['receiptNumber'], "_") + 1);
+    	$row['gender'] = $row['gender'] == 'm' ? $lang['male'] : $lang['female'];
+    	$row['category'] = $lang[$row['category']];
         $users[] = $row;
     }
 }
@@ -48,7 +49,7 @@ header('Content-Type: text/csv; charset=utf-8');
 header('Content-Disposition: attachment; filename=data.csv');
 $output = fopen('php://output', 'w');
 
-fputcsv($output, array($lang['name'], $lang['guardian'], $lang['permanentAddress'], $lang['temporaryAddress'], $lang['dob'], $lang['phone_number'], $lang['birth_place'], $lang['district'], $lang['ulb_region'], $lang['maritial_status'], $lang['receipt_number'], $lang['all_documents_provided'], $lang['remark'], $lang['special_preference']));
+fputcsv($output, array($lang['name'], $lang['guardian'], $lang['permanentAddress'], $lang['temporaryAddress'], $lang['dob'], $lang['phone_number'], $lang['birth_place'], $lang['district'], $lang['ulb_region'], $lang['category'], $lang['gender'], $lang['maritial_status'], $lang['receipt_number'], $lang['all_documents_provided'], $lang['special_preference'], $lang['remark']));
  
 if (count($users) > 0) {
     foreach ($users as $row) {
