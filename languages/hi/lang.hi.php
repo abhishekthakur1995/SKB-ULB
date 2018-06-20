6c7bd86b8339ce6ccd819917d39fed24540bbb86
abhishekthakur1995 <abhishekthakur.oneplus5@gmail.com>
2018-06-20 22:24:18 +0530

    Revert "#changes."
    This reverts commit 7e982e29e163a1502e3dc423971ba8ad7d05d48c.

--- languages/hi/lang.hi.php
+++ languages/hi/lang.hi.php
@@ -1,12 +1,12 @@
 <?php
-    
-    //form fields
-    $lang['name'] = 'नाम';
-    $lang['username'] = 'यूजरनाम';
+	
+	//form fields
+	$lang['name'] = 'नाम';
+  	$lang['username'] = 'यूजरनाम';
     $lang['password'] = 'पासवर्ड ';
-    $lang['phone_number'] = 'फ़ोन नंबर';
-    $lang['email'] = 'ईमेल आईडी';
-    $lang['ulb_region'] = 'निकाय क्षेत्र';
+  	$lang['phone_number'] = 'फ़ोन नंबर';
+  	$lang['email'] = 'ईमेल आईडी';
+  	$lang['ulb_region'] = 'निकाय क्षेत्र';
     $lang['maritial_status'] = 'वैवाहिक स्थिति';
     $lang['gender'] = 'लिंग';
     $lang['dob'] = 'जन्मतिथि';
@@ -28,7 +28,7 @@
     $lang['district'] = 'जिला';
     $lang['seed_number'] = 'सीड नंबर';
     $lang['update_record'] = 'विवरण अपडेट करें';
-    $lang['delete_record'] = 'आवेदक का रिकॉर्ड हटाएं';
+    $lang['delete_record'] = 'उम्मीदवार रिकॉर्ड हटाएं';
     $lang['receipt_number'] = 'आवेदक प्राप्ति क्रमांक';
 
     $lang['data_saved'] = 'डेटा सफलतापूर्वक दर्ज किया गया';
@@ -40,14 +40,14 @@
     $lang['form_status_2'] = 'विचारधीन';
 
     $lang['selected_for'] = 'श्रेणी';
-    $lang['special_preference'] = 'क्या आवेदक इनमे से कुछ है';
+    $lang['special_preference'] = 'क्या उम्मीदवार इनमे से कुछ है';
     $lang['EXOFFICER'] = 'भूतपूर्व सैनिक';
     $lang['DISABLED'] = 'दिव्यांग';
     $lang['SPORTSPERSON'] = 'खिलाड़ी';
     $lang['settings'] = 'सेटिंग्स';
     $lang['download'] = 'सूची डाउनलोड करें';
     $lang['tbl1_heading'] = 'आरक्षण तालिका';
-    $lang['tbl2_heading'] = 'आवेदक संख्या तालिका';
+    $lang['tbl2_heading'] = 'उम्मीदवार संख्या तालिका';
 
 
     //maritial_status
@@ -91,24 +91,24 @@
     //dashboard data
     $lang['dashboard_title'] = 'एडमिन डैशबोर्ड';
     $lang['welcome_msg_2'] = 'आपके डैशबोर्ड में आपका स्वागत है';
-    $lang['dashboard_btn_1'] = 'नया आवेदक विवरण दर्ज करें';
-    $lang['dashboard_btn_2'] = 'आवेदक का विवरण अपडेट करें';
-    $lang['dashboard_btn_3'] = 'आवेदक का चयन करें';
-    $lang['dashboard_btn_4'] = 'चयनित आवेदक रिपोर्ट जेनरेट करें';
+    $lang['dashboard_btn_1'] = 'नया उम्मीदवार विवरण दर्ज करें';
+    $lang['dashboard_btn_2'] = 'उम्मीदवार का विवरण अपडेट करें';
+    $lang['dashboard_btn_3'] = 'उम्मीदवारों का चयन करें';
+    $lang['dashboard_btn_4'] = 'चयनित उम्मीदवार रिपोर्ट जेनरेट करें';
     $lang['dashboard_btn_5'] = 'यहां क्लिक करे';
     $lang['dashboard_btn_6'] = 'यूएलबी पासवर्ड रीसेट करें';
-    $lang['dashboard_btn_7'] = 'आवेदक रिपोर्ट देखें और डाउनलोड करें';
-    $lang['dashboard_btn_8'] = 'आवेदक फॉर्म की स्थिति के आधार पर देखें';
+    $lang['dashboard_btn_7'] = 'उम्मीदवार रिपोर्ट देखें और डाउनलोड करें';
+    $lang['dashboard_btn_8'] = 'उम्मीदवार फॉर्म की स्थिति के आधार पर देखें';
     $lang['dashboard_btn_9'] = 'डुप्लिकेट डेटा';
     $lang['dashboard_btn_10'] = 'भूतपूर्व सैनिक/दिव्यांग/खिलाड़ी का चयन करे';
-    $lang['dashboard_btn_11'] = 'सभी वर्गों  के लिए आवेदकों  का चयन करें';
-    $lang['total_enteries'] = 'कुल आवेदक';
-    $lang['detailed_table'] = 'विस्तृत आवेदक टेबल ';
+    $lang['dashboard_btn_11'] = 'सभी वर्गों  के लिए उम्मीदवारों का चयन करें';
+    $lang['total_enteries'] = 'कुल उम्मीदवार';
+    $lang['detailed_table'] = 'विस्तृत उम्मीदवार टेबल';
     $lang['reservation_chart'] = 'आरक्षण चार्ट';
 
-    //create candidate form headings
-    $lang['enter_candidate_details'] = 'आवेदक विवरण दर्ज करें';
-    $lang['detail_header'] = 'आवेदक बनाने के लिए कृपया इस फॉर्म को भरें।';
+  	//create candidate form headings
+  	$lang['enter_candidate_details'] = 'उम्मीदवार विवरण दर्ज करें';
+  	$lang['detail_header'] = 'उम्मीदवार बनाने के लिए कृपया इस फॉर्म को भरें।';
 
     //login form headings
     $lang['login'] = 'लॉग इन करें';
@@ -117,12 +117,12 @@
     $lang['login_password_not_valid'] = 'आपके द्वारा डाला गया पासवर्ड वैध नहीं है';
 
     //edit candidate details page headings
-    $lang['candidates_details_heading'] = 'आवेदक का विवरण';
-    $lang['add_new_candidate_btn'] = 'नए आवेदक विवरण जोड़ें';
+    $lang['candidates_details_heading'] = 'उम्मीदवारों का विवरण';
+    $lang['add_new_candidate_btn'] = 'नए उम्मीदवार विवरण जोड़ें';
 
-    //general Erros
-    $lang['password_length_error'] = 'पासवर्ड में कम से कम 6 अक्षर होने चाहिए।';
-    $lang['password_mismatch_error'] = 'पासवर्ड और पासवर्ड की पुष्टि मेल नहीं खाते';
+  	//general Erros
+  	$lang['password_length_error'] = 'पासवर्ड में कम से कम 6 अक्षर होने चाहिए।';
+  	$lang['password_mismatch_error'] = 'पासवर्ड और पासवर्ड की पुष्टि मेल नहीं खाते';
     $lang['permanent_address_err'] = 'कृपया अपना स्थायी पता डाले';
     $lang['receipt_number_err'] = 'आवेदक प्राप्ति क्रमांक अनिवार्य है';
     $lang['duplicate_receipt_no'] = 'यह रसीद संख्या पहले से ही किसी अन्य उम्मीदवार के लिए दर्ज है';
@@ -136,31 +136,31 @@
     $lang['cancelButton'] = 'कैंसिल';
 
     //reset password
-    $lang['reset_password'] = 'पासवर्ड रिसेट करे';
+  	$lang['reset_password'] = 'पासवर्ड रिसेट करे';
     $lang['reset_password_header'] = 'अपना पासवर्ड रीसेट करने के लिए कृपया अपना विवरण भरें।';
     $lang['reset_password_msg_1'] = 'वर्तमान पासवर्ड';
     $lang['reset_password_msg_2'] = 'नया पासवर्ड';
     $lang['reset_password_msg_3'] = 'नए पासवर्ड की पुष्टि करें';
-    $lang['currentPasswodErrMsg_1'] = 'कृपया अपना वर्तमान पासवर्ड दर्ज करें';  
+  	$lang['currentPasswodErrMsg_1'] = 'कृपया अपना वर्तमान पासवर्ड दर्ज करें';  
 
-    $lang['update_detail_title'] = 'आवेदक के विवरण अपडेट करें';
+    $lang['update_detail_title'] = 'उम्मीदवार के विवरण अपडेट करें';
     $lang['save_success'] = 'डेटा सफलतापूर्वक दर्ज किया गया';
     $lang['update_success'] = 'डेटा सफलतापूर्वक अपडेट किया गया';
     $lang['reset_password_sucess'] = 'पासवर्ड सफलतापूर्वक रीसेट कर दिया गया है। कृपया अपने नए पासवर्ड से फिर से लॉगिन करें।';
     
-    $lang['all_documents_provided'] = 'आवेदक के फॉर्म की स्थिति';
+    $lang['all_documents_provided'] = 'उम्मीदवार के फॉर्म की स्थिति';
     $lang['experience_certificate'] = 'अनुभव प्रमाण पत्र';
     $lang['category_certificate'] = 'जाति प्रमाण पत्र';
     $lang['education_qualififcation_certificate'] = 'शैक्षिक योग्यता प्रमाण पत्र';
     $lang['marriage_certificate'] = 'विवाह प्रमाण पत्र';
-    $lang['all_cert'] = 'क्या आवेदक ने सभी आवश्यक दस्तावेज संलग्न किए हैं';
-    $lang['cert_header'] = 'आवेदक द्वारा प्रदान किए गए सभी दस्तावेजों का चयन करें / दस्तावेज की अनुपस्थि में टिप्पणी करे';
+    $lang['all_cert'] = 'क्या उम्मीदवार ने सभी आवश्यक दस्तावेज संलग्न किए हैं';
+    $lang['cert_header'] = 'उम्मीदवार द्वारा प्रदान किए गए सभी दस्तावेजों का चयन करें / दस्तावेज की अनुपस्थि में टिप्पणी करे';
     $lang['remark'] = 'टिप्पणी';
     $lang['search_candidates'] = 'आवेदक प्राप्ति क्रमांक से ढूंढे';
     $lang['no_match_found'] = 'कोई मेल नहीं मिला';
 
     $lang['alert_msg_1'] = 'सूचना !';
-    $lang['alert_msg_2'] = 'आवेदक का विवरण';
+    $lang['alert_msg_2'] = 'उम्मीदवार का विवरण';
     $lang['alert_msg_3'] = 'हिंदी';
     $lang['alert_msg_4'] = 'में ही प्रविष्ट करे';
     $lang['remark_place_holder'] = 'दस्तावेजों में पायी जाने वाली कमियों का उल्लेलख करे';
@@ -174,14 +174,14 @@
     $lang['apply_filter'] = 'डाटा फ़िल्टर करे';
     $lang['apply_filter_submit'] = 'फ़िल्टर करे ';
     $lang['close'] = 'बंद करे';
-    $lang['export_csv'] = 'सीएसवी के रूप में सभी आवेदक का  डेटा डाउनलोड करें';
+    $lang['export_csv'] = 'सीएसवी के रूप में सभी उम्मीदवार डेटा डाउनलोड करें';
 
-    $lang['candidate_already_selected'] = 'आवेदकों  को इस श्रेणी के लिए पहले ही चुना जा चुका है';
+    $lang['candidate_already_selected'] = 'उम्मीदवारों को इस श्रेणी के लिए पहले ही चुना जा चुका है';
     $lang['seed_already_used'] = 'यह सीड नंबर  का उपयोग पहले ही किया जा चूका है कृपया कोई दूसरा सीड नंबर का चयन करे';
     $lang['total_seats'] = 'कुल सीटें';
-    $lang['total_selected'] = 'कुल चयनित आवेदक ';
-    $lang['total_participated'] = 'कुल आवेदक ';
-    $lang['select_candidates'] = 'आवेदकों का चयन करें';
-    $lang['no_candidate_selected'] = 'कोई आवेदक  नहीं मिला';
+    $lang['total_selected'] = 'कुल चयनित उम्मीदवार';
+    $lang['total_participated'] = 'कुल उम्मीदवार';
+    $lang['select_candidates'] = 'उम्मीदवारों का चयन करें';
+    $lang['no_candidate_selected'] = 'कोई उम्मीदवार नहीं मिला';
 
 ?>
