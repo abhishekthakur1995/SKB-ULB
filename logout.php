38dc0da41c8760224ccff994aca7642f7a82e031
Abhishek Thakur <abhishek.thakur@infoobjects.com>
2018-06-20 12:45:51 +0530

    Revert "#change session id on logout"
    This reverts commit e03b057d7fc0ee33885e9427f8b31cf349e03d6b.

--- logout.php
+++ logout.php
@@ -1,12 +1,15 @@
 <?php
+// Initialize the session
 session_start();
  
+// Unset all of the session variables
 $_SESSION = array();
  
+// Destroy the session.
 session_destroy();
 session_unset();
-session_regenerate_id();
  
+// Redirect to login page
 header("location: index.php");
 exit;
 ?>
