e03b057d7fc0ee33885e9427f8b31cf349e03d6b
Abhishek Thakur <abhishek.thakur@infoobjects.com>
2018-06-20 12:45:14 +0530

    #change session id on logout

--- logout.php
+++ logout.php
@@ -1,15 +1,12 @@
 <?php
-// Initialize the session
 session_start();
  
-// Unset all of the session variables
 $_SESSION = array();
  
-// Destroy the session.
 session_destroy();
 session_unset();
+session_regenerate_id();
  
-// Redirect to login page
 header("location: index.php");
 exit;
 ?>
