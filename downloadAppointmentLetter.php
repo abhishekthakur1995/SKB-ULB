25a922dfa22a9ac2841776fc105b581756ba525d
abhishek <coach.ab9@gmail.com>
2018-07-03 16:14:16 +0530

    Create downloadAppointmentLetter.php

---
+++ downloadAppointmentLetter.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+require('config.php');
+require('languages/hi/lang.hi.php');
+require('common/common.php');
+
+if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
+    header("location: index.php");
+    exit;
+} else {
+    if (time()-$_SESSION['timestamp'] > IDLE_TIME) {
+        header("location: logout.php");
+    }   else{
+        $_SESSION['timestamp']=time();
+    }
+}
+
+?>
