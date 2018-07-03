6fba56a916ba395268c338ed27efd4f0d675c45d
abhishek <coach.ab9@gmail.com>
2018-07-03 19:29:23 +0530

    Update downloadAppointmentLetter.php

--- downloadAppointmentLetter.php
+++ downloadAppointmentLetter.php
@@ -4,6 +4,11 @@ require('config.php');
 require('languages/hi/lang.hi.php');
 require('common/common.php');
 require('vendor/autoload.php');
+require('Mustache/Autoloader.php');
+Mustache_Autoloader::register();
+$mustache = new Mustache_Engine(array(
+    'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/templates') 
+));
 $mpdf = new mPDF();
 
 if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
@@ -12,12 +17,18 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
 } else {
     if (time()-$_SESSION['timestamp'] > IDLE_TIME) {
         header("location: logout.php");
-    }   else{
+    } else {
         $_SESSION['timestamp']=time();
     }
 }
 
-$mpdf->WriteHTML('Hello World');
+header('Content-Type: application/pdf');
+for($i=0; $i<5; $i++) {
+	$template = $mustache->loadTemplate('letter');
+	$html = $template->render();
+	$mpdf->WriteHTML("नाम");
+	$mpdf->AddPage();
+}
+header('Content-Type: application/pdf');
 $mpdf->Output();
-
 ?>
