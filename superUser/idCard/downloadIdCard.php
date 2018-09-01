900c761265d07c9c91281ac2b5212fdb66c85235
abhishek_thakur <coach.ab9@gmail.com>
2018-09-01 15:46:29 +0530

    #id card changes.

--- superUser/idCard/downloadIdCard.php
+++ superUser/idCard/downloadIdCard.php
@@ -5,46 +5,72 @@
     require('../../config.php');
     require('../../languages/hi/lang.hi.php');
     require('../../common/common.php');
+    require('../../vendor/autoload.php');
+    
+    function generatePDF() {
+        $mpdf = new mPDF('utf-8', 'A4-C');
+        $mpdf->SetImportUse();
+        $mpdf->text_input_as_HTML = TRUE;
+        define('_MPDF_TTFONTDATAPATH', sys_get_temp_dir()."/");
 
-    $data = Common::getCandidatesForIdCard();
-    mkdir($data[0]['ulbRegion'], 0777, true);
-    for($i=1000; $i<1947; $i++) {
-        $image = new Imagick("image/img.jpeg");
-        $draw = new ImagickDraw();
+        $stylesheet = file_get_contents('../../css/letter.css');
+        $data = Common::getCandidatesForIdCard();
 
-        /* Font properties */
-        $draw->setFont('fonts/MANGAL.TTF');
-        $draw->setFontSize(30);
-        $draw->setTextEncoding('UTF-8');
+        for($i=0; $i<100; $i++) {
+            $pagecount = $mpdf->SetSourceFile('image/img.pdf');
+            $tplId = $mpdf->ImportPage($pagecount);
+            $mpdf->UseTemplate($tplId);
+            
+            $mpdf->WriteHTML($stylesheet, 1);
 
-        /* Create text */
-        $address = Common::getFormattedAddress($data[$i]['permanentAddress']);
-        $line1 = $address[0]." ".$address[1]." ".$address[2]." ".$address[3]." ".$address[4];
-        $line2 = $address[5]." ".$address[6]." ".$address[7]." ".$address[8]." ".$address[9];
-        $image->annotateImage($draw, 320, 740, 0, $data[$i]['name']);
-        $image->annotateImage($draw, 320, 840, 0, $data[$i]['guardian']);
-        $image->annotateImage($draw, 320, 940, 0, $line1);
-        $image->annotateImage($draw, 60, 1040, 0, $line2);
-        $image->annotateImage($draw, 320, 1140, 0, $data[$i]['ulbRegion']);
+            $address = Common::getFormattedAddress($data[$i]['permanentAddress']);
+            $line1 = $address[0]." ".$address[1]." ".$address[2]." ".$address[3]." ".$address[4]."".$address[5];
+            $line2 = $address[6]." ".$address[7]." ".$address[8]." ".$address[9]." ".$address[10];
 
-        /* Give image a format */
-        $image->setImageFormat('png');
-        $image->minifyImage();
-        
-        header('Content-type: image/png');
-        file_put_contents($data[0]['ulbRegion']."/imagick_ouput_".$i.".png" , $image);
+            $mpdf->WriteFixedPosHTML($data[$i]['name'], 70, 162, 100, 100, 'hidden');
+            $mpdf->WriteFixedPosHTML($data[$i]['guardian'], 70, 262, 100, 100, 'hidden');
+            $mpdf->WriteFixedPosHTML($line1, 70, 262, 100, 100, 'hidden');
+            $mpdf->WriteFixedPosHTML($line2, 70, 362, 100, 100, 'hidden');
+            $mpdf->WriteFixedPosHTML($data[$i]['ulbRegion'], 70, 462, 100, 100, 'hidden');
+
+            if($i != sizeof($data)-1) {
+                $mpdf->AddPage();
+            }
+        }
+        $mpdf->Output('id.pdf', 'D');
     }
 
+    function generateImg() {
+        $data = Common::getCandidatesForIdCard();
+        mkdir($data[0]['ulbRegion'], 0777, true);
+        for($i=1000; $i<1947; $i++) {
+            $image = new Imagick("image/img.jpeg");
+            $draw = new ImagickDraw();
 
-    // $path = Yii::app()->basePath."/imagick.jpeg";
-    // $img = new Imagick($path);
-    // $img->resizeImage(80,80, imagick::FILTER_LANCZOS, 0.9, true);
-    // $img->cropImage (80,80,0,0);
-    // $img->writeImage(Yii::app()->basePath."/imagick1.jpeg");
-    // pr($img->getImageGeometry());
+            /* Font properties */
+            $draw->setFont('fonts/MANGAL.TTF');
+            $draw->setFontSize(30);
+            $draw->setTextEncoding('UTF-8');
 
-    //To get attachment
-    // header('Content-Disposition: attachment; filename=data.png');
+            /* Create text */
+            $address = Common::getFormattedAddress($data[$i]['permanentAddress']);
+            $line1 = $address[0]." ".$address[1]." ".$address[2]." ".$address[3]." ".$address[4];
+            $line2 = $address[5]." ".$address[6]." ".$address[7]." ".$address[8]." ".$address[9];
+            $image->annotateImage($draw, 320, 740, 0, $data[$i]['name']);
+            $image->annotateImage($draw, 320, 840, 0, $data[$i]['guardian']);
+            $image->annotateImage($draw, 320, 940, 0, $line1);
+            $image->annotateImage($draw, 60, 1040, 0, $line2);
+            $image->annotateImage($draw, 320, 1140, 0, $data[$i]['ulbRegion']);
 
-    //$draw->setFont('Bookman-DemiItalic');
+            /* Give image a format */
+            $image->setImageFormat('png');
+            $image->minifyImage();
+            
+            header('Content-type: image/png');
+            file_put_contents($data[0]['ulbRegion']."/imagick_ouput_".$i.".png" , $image);
+        }
+    }
+
+    generatePDF();
+
 ?>
